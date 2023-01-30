4dfba1893a30368d0cc7ecf5f6da1b7f928ffabd
username <email@blahblah.com>
2023-01-29 20:39:51 -0500

    Initial Commit

---
+++ Connection/ConnectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionApp.swift
+//  Connection
+//
+//  Created by Khalid Thomas on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
